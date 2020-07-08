3f7ad6f8f4adb0f5768f486590ec61e630886107
Osa <osaimola@gmail.com>
2020-07-08 10:59:36 -0600

    add timestapms; add an empty pending area

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,45 @@
 const inputField = document.getElementById("newtaskfield");
 const submitButton = document.getElementById("submittask");
+const pendingToDoList = document.querySelector("body>.pending-todo>ul");
 const activeToDoList = document.querySelector("body>.active-todo>ul");
 const completedToDoList = document.querySelector("body>.completed-todo>ul");
+// dictionary will store {"todo item" : [start time, end time, last edited time]}
 const todos = {};
 
 // APPENDING A CHILD THAT EXISTS UNDER ONE PARENT TO ANOTHER PARENT SHOULD MOVE IT TO THe NEW PARENT
 
 submitButton.addEventListener("click", createToDo);
 
-// Get user input from the new task form and create a new TO DO ITEM
-function createToDo(event) {
+// GEt user input from the new task form and create a new Pending to do
+function addPendingToDo(event) {
   event.preventDefault();
+
   let userInput = inputField.value.trim();
+  // TODO : check if input exists in todos and prevent duplicate items from being added
   if (userInput === "") {
+    // TODO : provide visual feedback about bad input
     return;
   }
 
   inputField.value = null;
-  // TODO : use the input field value to update the to do list
+}
+
+// Get user input from the new task form and create a new TO DO ITEM
+function createToDo(event, userInput) {
+  event.preventDefault();
+
+  let userInput = inputField.value.trim();
+  // TODO : check if input exists in todos and prevent duplicate items from being added
+  if (userInput === "") {
+    // TODO : provide visual feedback about bad input
+    return;
+  }
+
+  inputField.value = null;
+
+  todos[userInput] = [new Date()];
+
+  // create a new list item
   let newItem = document.createElement("li");
   newItem.setAttribute("id", userInput);
 
@@ -30,9 +52,16 @@ function createToDo(event) {
   newLabel.setAttribute("for", userInput);
   newLabel.innerHTML = userInput;
 
+  // create additional info elements
+  let newStartTime = document.createElement("p");
+  newStartTime.innerText = todos[userInput][0].toLocaleString();
+  let lineBreak = document.createElement("br");
+
   // add the checlist to the created item
   newItem.appendChild(newInput);
   newItem.appendChild(newLabel);
+  newItem.appendChild(lineBreak);
+  newItem.appendChild(newStartTime);
 
   newItem.addEventListener("click", () => {
     completeToDo(event, newItem);
@@ -44,5 +73,15 @@ function createToDo(event) {
 
 function completeToDo(event, element) {
   event.preventDefault();
+
+  // add the todo completed time.
+  let toDoStarted = todos[element.getAttribute("id")][0];
+  let toDoEnded = new Date();
+  todos[element.getAttribute("id")][1] = toDoEnded;
+
+  let newEndTime = document.createElement("p");
+  newEndTime.innerText = toDoEnded.toLocaleString();
+
   completedToDoList.appendChild(element);
+  element.appendChild(newEndTime);
 }
